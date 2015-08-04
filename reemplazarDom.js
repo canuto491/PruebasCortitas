768111043e1a9104aa2342ea8a384c24cb1f71ce
Luis Sousa <fenix_gry@hotmail.com>
2015-08-04 16:40:23 +0100

    Prueba getElementByName y de Replace de MetaTag
    Prueba de la alteración de una etiqueta meta del DOM y de la selección
    de un elemento por su nombre y no por su id

--- reemplazarDom.js
+++ reemplazarDom.js
@@ -1,9 +1,9 @@
 function reemplazar(){
-				var nuevoElementoDelDOM = document.createElement("h1");
-				var textoDelNuevoElemento = document.createTextNode("Reemplazo del texto");
+				var nuevoElementoDelDOM = document.createElement("meta");
+				var textoDelNuevoElemento = document.createTextNode('content="summary_large_imageREPLACE"');
 				nuevoElementoDelDOM.appendChild(textoDelNuevoElemento);
 				var miDiv = document.getElementById("div1");
-				var viejoP = document.getElementById("p1");
+				var viejoP = document.getElementsByName("twitter:card");
 				miDiv.replaceChild(nuevoElementoDelDOM,viejoP);
 			}
 // JavaScript Document
